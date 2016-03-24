ab382784ee5035a718f0f9250c129063ff429325
Timothy <timothy.ejohansson@gmail.com>
2016-03-24 16:22:30 +0100

    upgrade to express sdk 3 and also make it work with mobile apps as an api

--- server.js
+++ server.js
@@ -7,64 +7,53 @@ var stormpath = require('express-stormpath');
 var path = require('path');
 var fs = require('fs');
 
+var SPA_ROOT = process.argv[2]; // The path to the SPA app to serve.
 var app = express();
-
-var SPA_ROOT = process.argv[2]; // The path to the SPA app to serve
 
 function isValidPath(path) {
   try {
     return path && fs.lstatSync(SPA_ROOT).isDirectory() === true;
-  } catch(error) {
+  } catch (error) {
     return false;
   }
 }
 
-if (!isValidPath(SPA_ROOT)) {
-  console.error('Error: First argument must be a valid path to your SPA app.');
-  return 1;
+if (SPA_ROOT) {
+  if (!isValidPath(SPA_ROOT)) {
+    console.error('Error: First argument must be a valid path to your SPA app.');
+    return 1;
+  }
 }
 
 app.use(stormpath.init(app, {
   web: {
-    login: {
-      enabled: true
-    },
-    logout: {
-      enabled: true
-    },
-    register: {
-      enabled: true
-    },
+    produces: ['application/json'],
     me: {
-      enabled: true
-    },
-    spaRoot: path.join(SPA_ROOT, 'index.html')
+      expand: {
+        customData: true,
+        groups: true,
+        groupMemberships: true
+      }
+    }
   }
 }));
 
-app.use(express.static(SPA_ROOT));
+if (SPA_ROOT) {
+  app.use(express.static(SPA_ROOT));
 
-// Example of a protected route which requires login
-app.get('/secret', stormpath.loginRequired, function(req, res) {
-  /*
-    If we get here, the user is logged in. Otherwise, they
-    were redirected to the login page
-   */
-  res.send({ result: true });
-});
+  // All undefined asset or api routes should return a 404.
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(function (req, res) {
+    res.status(404).end();
+  });
 
-// All undefined asset or api routes should return a 404
-app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(function(req, res) {
-  res.status(404).end();
-});
+  // All other routes should redirect to the index.html.
+  app.route('/*').get(function (req, res) {
+    res.sendFile(path.join(SPA_ROOT, 'index.html'));
+  });
+}
 
-// All other routes should redirect to the index.html
-app.route('/*').get(function(req, res) {
-  res.sendFile(path.join(SPA_ROOT, 'index.html'));
-});
-
-app.on('stormpath.ready', function() {
-  app.listen(3000, function() {
-    console.log('Stormpath SPA Development Server listening at http://localhost:3000');
+app.on('stormpath.ready', function () {
+  app.listen(3000, function () {
+    console.log('Stormpath SPA Development Server listening at http://localhost:3000.');
   });
 });
